66a4975f2ee8dc3995fb7dc82616764d94c48312
Satya <satyanarayanamylavarapuaws@gmail.com>
2025-07-29 22:06:57 +0530

    iam

--- terraform/iam.tf
+++
@@ -1,8 +0,0 @@
-# Create an IAM user named "sample"
-resource "aws_iam_user" "sample_user" {
-  name = "sany"
-  tags = {
-    Environment = "Development"
-    Project     = "TerraformUserManagement"
-  }
-}
