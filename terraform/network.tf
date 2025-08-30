e35ce10ab2a143755f14124fc430e2993f9248bf
Satya <satyanarayanamylavarapuaws@gmail.com>
2025-08-30 10:26:28 +0530

    remove vpc

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,6 +1,6 @@
 #start vpc module
 
-resource "aws_vpc" "dev-vpc" {
+/* resource "aws_vpc" "dev-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -8,7 +8,7 @@ resource "aws_vpc" "dev-vpc" {
     Name        = "dev-vpc"
     Environment = "development"
   }
-}
+} */
 
 
 #end vpc module
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,41 @@
+variable "namespace" {
+  description = "The namespace for all resources"
+  type        = string
+
+}
+
+variable "global_tags" {
+  description = "A map of tags to apply to all resources"
+  type        = map(any)
+}
+
 variable "region" {
   description = "The AWS region to deploy resources in"
   type        = string
   default     = "us-east-1"
 }
 
-  
+
+variable "region_secondary" {
+  description = "The secondary AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-2"
+}
+
+# Variables for VPC
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "aws_azs" {
+  description = "A list of availability zones for the VPC"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
