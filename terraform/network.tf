b664c6f44ba4f7ef9b6b66e652a4db82ff311720
Satya <satyanarayanamylavarapuaws@gmail.com>
2025-08-30 10:06:12 +0530

    added vpc

---
+++ terraform/network.tf
@@ -0,0 +1,14 @@
+#start vpc module
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "dev-vpc"
+    Environment = "development"
+  }
+}
+
+
+#end vpc module
